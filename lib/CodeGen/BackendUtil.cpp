7e42e2a06ee0ca5d24f544ffb9f959bcf9ddccf8
George Stelle <stelleg@lanl.gov>
2018-06-04 09:08:24 -0600

    Initial qthreads backend support

--- lib/CodeGen/BackendUtil.cpp
+++ lib/CodeGen/BackendUtil.cpp
@@ -57,8 +57,11 @@
 #include "llvm/Transforms/ObjCARC.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Scalar/GVN.h"
-#include "llvm/Transforms/Utils.h"
-#include "llvm/Transforms/Tapir/LoweringUtils.h"
+#include "llvm/Transforms/Tapir/TapirTypes.h"
+#include "llvm/Transforms/Tapir/TapirUtils.h"
+#include "llvm/Transforms/Tapir/CilkABI.h"
+#include "llvm/Transforms/Tapir/OpenMPABI.h"
+#include "llvm/Transforms/Tapir/QthreadsABI.h"
 #include "llvm/Transforms/Utils/NameAnonGlobals.h"
 #include "llvm/Transforms/Utils/SymbolRewriter.h"
 #include <memory>
@@ -543,6 +546,23 @@ void EmitAssemblyHelper::CreatePasses(legacy::PassManager &MPM,
   }
 
   PMBuilder.OptLevel = CodeGenOpts.OptimizationLevel;
+
+  switch(LangOpts.Tapir){
+    case TapirTargetType::Cilk:
+      PMBuilder.tapirTarget = new llvm::CilkABI();
+      break;
+    case TapirTargetType::OpenMP:
+      PMBuilder.tapirTarget = new llvm::OpenMPABI();
+      break;
+    case TapirTargetType::Qthreads:
+      PMBuilder.tapirTarget = new llvm::QthreadsABI();
+      break;
+    case TapirTargetType::Serial:
+      assert(0 && "TODO MAKE OTHER TAPIR OPTS");
+    case TapirTargetType::None:
+      PMBuilder.tapirTarget = nullptr;
+      break;
+  }
 
   if (LangOpts.Detach) PMBuilder.DisableTapirOpts = true;
   if (LangOpts.Rhino) PMBuilder.Rhino = true;
